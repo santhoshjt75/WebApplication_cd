ba0528177ff8b28c3351acafe26bf600bfab59af
santhoshjt75 <santhoshjt@yahoo.com>
2016-07-28 15:39:18 +0530

    Add project files.

---
+++ WebApplication_cd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_cd.Startup))]
+namespace WebApplication_cd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
